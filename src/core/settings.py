d366154f858a684693c0d18fdf364edb9c523eaf
Jon-Frost <jon@thefrosts.net>
2026-03-13 15:56:41 +0000

    documentation

--- src/core/settings.py
+++ src/core/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # CUSTOM CONTEXT PROCESSOR - INJECTS BASKET ITEM COUNT INTO EVERY TEMPLATE
+                # INJECTS BASKET ITEM COUNT INTO EVERY TEMPLATE
                 'marketplace.global_context.basket_count',
             ],
         },
